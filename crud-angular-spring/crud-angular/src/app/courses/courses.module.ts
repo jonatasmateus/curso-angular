6cff5f91d439d06facbcce5ce725fc3f376c6eaa
Jonatas Mateus <jonatasmateuscontato@gmail.com>
2023-04-21 19:48:07 -0300

    crud aula 07

--- crud-angular-spring/crud-angular/src/app/courses/courses.module.ts
+++ crud-angular-spring/crud-angular/src/app/courses/courses.module.ts
@@ -1,11 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatListModule } from '@angular/material/list';
-import { MatTableModule } from '@angular/material/table';
 
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
-
 
 @NgModule({
   declarations: [
@@ -14,8 +12,7 @@ import { CoursesComponent } from './courses/courses.component';
   imports: [
     CommonModule,
     CoursesRoutingModule,
-    MatListModule,
-    MatTableModule
+    AppMaterialModule
   ]
 })
 export class CoursesModule { }
