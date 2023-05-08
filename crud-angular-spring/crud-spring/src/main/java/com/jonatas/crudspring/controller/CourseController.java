4543437df3a55a9a0cbd37543d8664d2bb85916a
Jonatas Mateus <jonatasmateuscontato@gmail.com>
2023-05-08 10:33:33 -0300

    crud aula 15

---
+++ crud-angular-spring/crud-spring/src/main/java/com/jonatas/crudspring/controller/CourseController.java
@@ -0,0 +1,26 @@
+package com.jonatas.crudspring.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jonatas.crudspring.model.Course;
+import com.jonatas.crudspring.repository.CourseRepository;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/courses")
+@AllArgsConstructor
+public class CourseController {
+    
+    private final CourseRepository courseRepository;
+
+    //@RequestMapping(method = RequestMethod.GET)
+    @GetMapping
+    public List<Course> list() {
+        return courseRepository.findAll();
+    }
+}
