767b28de56b9f16dd14d68b120325bca8dbea639
Jonatas Mateus <jonatasmateuscontato@gmail.com>
2023-05-08 13:21:07 -0300

    fix warning message

--- crud-angular-spring/crud-spring/src/main/java/com/jonatas/repository/CourseRepository.java
+++ crud-angular-spring/crud-spring/src/main/java/com/jonatas/repository/CourseRepository.java
@@ -1,11 +1,11 @@
 package com.jonatas.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+// import org.springframework.stereotype.Repository;
 
 import com.jonatas.model.Course;
 
-@Repository
+// @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
     
 }
